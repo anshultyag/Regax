bddf84eb67e21945e720723950fb5c8a2a6a6a00
anshultyag <you@example.com>
2022-01-26 19:45:06 +0530

    Added: User enter a valid email

--- src/com/company/Regax.java
+++ src/com/company/Regax.java
@@ -4,13 +4,18 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Regax {
-          public static boolean isValidUsername(String fname)
-        {
+        public static boolean isValidUsername(String fname){
             String regex = "^[A-Z][a-z]{2,}$";
             Pattern p = Pattern.compile(regex);
             Matcher m = p.matcher(fname);
             return m.matches();
-        }
+          }
+       public static boolean isValidEmail(String email) {
+        String regex = "^abc[.a-z]+@bl[.]co[.a-z]+";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(email);
+        return m.matches();
+         }
         public static void main(String[] args){
             Scanner sc = new Scanner(System.in);
             System.out.println("Please enter your First name : ");
@@ -19,6 +24,9 @@ public class Regax {
             String str2 = sc.nextLine();
             System.out.println(isValidUsername(str1));
             System.out.println(isValidUsername(str2));
-            
+
+            System.out.println("Please enter your Email : ");
+            String str3 = sc.nextLine();
+            System.out.println(isValidEmail(str1));
         }
     }
